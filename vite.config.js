c0e438a3f5a8ed0bec99875b02bec35e31f194f4
Dave-29 <159033291+Dave-29@users.noreply.github.com>
2024-07-11 16:39:35 +0200

    building changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/scss/style.scss";`
+      }
+    }
+  },
+  server: {
+    watch: {
+      paths: ['src/styles/css/**/*.css']
+    }
+  }
+});
